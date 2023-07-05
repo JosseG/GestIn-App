7b01526b01c56827b0eed38036dbb536aea5405b
JoseGQ <jq994435258@gmail.com>
2023-07-05 06:50:16 -0500

    update manager added

--- App/GestInApp/app/src/main/java/com/nsgej/gestinapp/ui/MainActivity.kt
+++ App/GestInApp/app/src/main/java/com/nsgej/gestinapp/ui/MainActivity.kt
@@ -1,13 +1,16 @@
 package com.nsgej.gestinapp.ui
 
+import android.app.Activity
 import android.content.DialogInterface.BUTTON_POSITIVE
 import android.content.Intent
 import android.os.Bundle
 import android.util.Log
 import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import com.google.android.play.core.appupdate.AppUpdateManagerFactory
+import com.google.android.play.core.appupdate.AppUpdateOptions
 import com.google.android.play.core.install.model.AppUpdateType
 import com.google.android.play.core.install.model.UpdateAvailability
 import com.google.firebase.firestore.ktx.firestore
@@ -22,78 +25,72 @@ class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
 
-    private  var dialog : AlertDialog? = null;
+    private var dialog: AlertDialog? = null;
 
     val db = Firebase.firestore
 
     private val REQUEST_CODE = 11
 
 
-
-
-
-
-
-
-
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        Log.i("Inicio", "Empezandooo")
+        val appUpdateManager = AppUpdateManagerFactory.create(this.applicationContext)
+        val appUpdateInfoTask = appUpdateManager.appUpdateInfo
 
-
-        Log.i("Inicio","Empezandooo")
 
         binding = ActivityMainBinding.inflate(layoutInflater)
 
         setContentView(binding.root)
 
 
+        val activityResult =
+            registerForActivityResult(ActivityResultContracts.StartIntentSenderForResult()) { result ->
+                when (result.resultCode) {
+                    Activity.RESULT_OK -> {
+                        Log.i("UPDATE TEST", "Todo ok");
+                    }
+                    else -> {
+                        Log.i("UPDATE TEST", "No se completó")
+                    }
+                }
+            }
 
 
-        val appUpdateManager = AppUpdateManagerFactory.create(this)
-
-        // Returns an intent object that you use to check for an update.
-        val appUpdateInfoTask = appUpdateManager.appUpdateInfo
-
-
-
-// Checks that the platform will allow the specified type of update.
         appUpdateInfoTask.addOnSuccessListener { appUpdateInfo ->
             if (appUpdateInfo.updateAvailability() == UpdateAvailability.UPDATE_AVAILABLE && appUpdateInfo.isUpdateTypeAllowed(
-                    AppUpdateType.IMMEDIATE)
+                    AppUpdateType.IMMEDIATE
+                )
             ) {
                 try {
-                    appUpdateManager.startUpdateFlowForResult(appUpdateInfo,AppUpdateType.IMMEDIATE,this,REQUEST_CODE)
-                }catch (e : Exception){
-                    Log.i("E",e.toString())
+                    appUpdateManager.startUpdateFlowForResult(
+                        appUpdateInfo,
+                        activityResult,
+                        AppUpdateOptions.newBuilder(AppUpdateType.IMMEDIATE)
+                            .setAllowAssetPackDeletion(true)
+                            .build()
+                    )
+                } catch (e: Exception) {
+                    Log.i("ERROR LISTENER UPDATE", e.toString())
                 }
             }
         }
 
-
-
-
-
-
-
-
-
-
         db.collection("state")
             .addSnapshotListener { value, error ->
                 if (value != null) {
-                    for(document in value.documents){
+                    for (document in value.documents) {
 
-                        if(document.data?.get("isActive")==true){
+                        if (document.data?.get("isActive") == true) {
 
                             dismissUnderMaintenanceDialog()
 
-                        }else{
+                        } else {
                             showUnderMaintenanceDialog("Está en mantenimiento")
                         }
                     }
-                }else{
-                    showUnderMaintenanceDialog("Existe un error")
+                } else {
+                    showUnderMaintenanceDialog("Existe un error $error")
                 }
             }
 
@@ -103,18 +100,13 @@ class MainActivity : AppCompatActivity() {
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
-        if(requestCode == REQUEST_CODE){
-            Toast.makeText(this,"Empezando descarga",Toast.LENGTH_SHORT)
-            if(resultCode != RESULT_OK){
+        if (requestCode == REQUEST_CODE) {
+            Toast.makeText(this, "Empezando descarga", Toast.LENGTH_SHORT).show()
+            if (resultCode != RESULT_OK) {
                 Log.i("Error update", "La actualización falló ".plus(resultCode))
             }
         }
     }
-
-
-
-
-
 
 
     private fun showUnderMaintenanceDialog(msj: String) {
@@ -132,11 +124,8 @@ class MainActivity : AppCompatActivity() {
         }
         dialog!!.setMessage(msj)
 
-
         if (!this.isFinishing) {
             dialog!!.show()
-
-
         }
     }
 
